c2ebf8041c847373770214d19821db78ca0bde14
Anton Adam <116871473+aent0n@users.noreply.github.com>
2026-06-20 00:11:29 +0200

    Implement of a function to collect active TCP and UDP connections.

--- triage.ps1
+++ triage.ps1
@@ -59,3 +59,44 @@ function Get-triageProcesses {
     }
     return $processlist
 }
+
+function Get-TriageNetwork {
+    <# .SYNOPSIS
+    collecte les infos réseau tcp/udp#>
+
+    Write-Host "[+] gathering network connections" -ForegroundColor Cyan
+    
+    $networkList = [System.Collections.Generic.List[PSCustomObject]]::new()
+    
+    $tcpNetConns = Get-NetTCPConnection | Where-Object { $_.RemoteAddress -ne "127.0.0.1" -and $_.RemoteAddress -ne "::1" }
+    foreach ($conn in $tcpNetConns) {
+        $networkList.Add([PSCustomObject]@{
+                Protocol      = "TCP"
+                LocalAddress  = $conn.LocalAddress
+                LocalPort     = $conn.LocalPort
+                RemoteAddress = $conn.RemoteAddress
+                RemotePort    = $conn.RemotePort
+                State         = $conn.State
+                PID           = $conn.OwningProcess
+            })
+    }
+
+    $udpNetConns = Get-NetUDPEndpoint | Where-Object { $_.LocalAddress -ne "127.0.0.1" -and $_.LocalAddress -ne "::1" }
+    foreach ($conn in $udpNetConns) {
+        $networkList.Add([PSCustomObject]@{
+                Protocol      = "UDP"
+                LocalAddress  = $conn.LocalAddress
+                LocalPort     = $conn.LocalPort
+                RemoteAddress = $conn.RemoteAddress
+                RemotePort    = $conn.RemotePort
+                State         = $conn.State
+                PID           = $conn.OwningProcess
+            })
+    }
+
+    return $networkList
+}
+
+
+#Get-TriageSystemInfo
+#Get-TriageProcesses
