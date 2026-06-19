3b40a954bc65ee1f0a3c239583aaad4d68715601
Anton Adam <116871473+aent0n@users.noreply.github.com>
2026-06-19 23:58:09 +0200

    Implementation of a function to collect process details and SHA-256 hashes

--- triage.ps1
+++ triage.ps1
@@ -25,4 +25,37 @@ function Get-TriageSystemInfo {
     
 }
     
-Get-TriageSystemInfo | Format-List
+function Get-triageProcesses {
+    
+    <# .SYNOPSIS
+    collecte les processus actifs #>
+
+    Write-Host "[+] gathering processes..." -ForegroundColor Cyan
+    $processes = Get-CimInstance -ClassName Win32_Process
+    $processlist = [System.Collections.Generic.List[PSCustomObject]]::new()
+
+    foreach ($proc in $processes) {
+     
+        $sha256 = "N/A"
+        if ($proc.ExecutablePath) {
+            try {
+                $hashResult = Get-FileHash -Path $proc.ExecutablePath -Algorithm SHA256 -ErrorAction Stop
+                $sha256 = $hashResult.Hash
+            }
+            catch {
+                $sha256 = "permission denied or locked"
+            }
+        }
+
+        $processlist.Add([PSCustomObject]@{
+                PID            = $proc.ProcessId
+                PPID           = $proc.ParentProcessId
+                Name           = $proc.Name
+                CommandLine    = $proc.CommandLine
+                ExecutablePath = $proc.ExecutablePath
+                SHA256         = $sha256
+                CreationDate   = $proc.CreationDate
+            })
+    }
+    return $processlist
+}
